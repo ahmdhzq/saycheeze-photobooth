b75327b1b5fd8c1dea1502de7475e901ebb36367
ahmdhzq <haziexahmad18@gmail.com>
2025-09-03 16:27:23 +0700

    fix: navbar

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@
 
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from '../components/Navbar';
 import { Analytics } from '@vercel/analytics/react'; // <-- 1. Tambahkan import ini
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,7 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50`}>
-        <Navbar />
         {children}
         <Analytics /> 
       </body>
