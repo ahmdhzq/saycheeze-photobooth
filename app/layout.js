22642dd45d093c809d7245daa3a44dfa80710948
ahmdhzq <haziexahmad18@gmail.com>
2025-09-03 16:25:12 +0700

    feat: Integrate Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 // app/layout.js
+
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Navbar from '../components/Navbar';
+import { Analytics } from '@vercel/analytics/react'; // <-- 1. Tambahkan import ini
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50`}>
+        <Navbar />
         {children}
+        <Analytics /> 
       </body>
     </html>
   );
